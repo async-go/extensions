8f55d5b0bf4d672a4faeb9bbcf01f9f34b0347c0
Jason Yavorska <jason@metaluna.io>
2021-04-26 11:09:27 +0200

    Add basic extension

---
+++ firefox/asyncgo.js
@@ -0,0 +1,45 @@
+// Hardcode team ID for now
+// TODO: make configurable
+asyncgo_team_id = 1
+
+function onCreated() {
+  if (browser.runtime.lastError) {
+    console.log(`Error: ${browser.runtime.lastError}`);
+  } else {
+    console.log("Item created successfully");
+  }
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+browser.menus.create({
+  id: "save-selection",
+  title: "Create topic from selection",
+  contexts: ["selection"]
+}, onCreated);
+
+browser.menus.create({
+  id: "save-page",
+  title: "Create topic from page",
+  contexts: ["page"]
+}, onCreated);
+
+function buildUrl(context, selection) {
+   return 'https://app.asyncgo.com/teams/' + asyncgo_team_id + '/topics/new?context="' + encodeURIComponent(context) + '"&selection="' + encodeURIComponent(selection) + '"'
+}
+
+browser.menus.onClicked.addListener((info, tab) => {
+  switch (info.menuItemId) {
+    case "save-selection":
+      browser.tabs.create({url: buildUrl(info.pageUrl, info.selectionText)})
+      console.log(info.pageUrl);
+      console.log(info.selectionText);
+      break;
+    case "save-page":
+      browser.tabs.create({url: buildUrl(info.pageUrl, '')})
+      console.log(info.pageUrl);
+      break;
+  }
+});
