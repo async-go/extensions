668eaa083acc4722683551409a7a857eb732daa3
Jason Yavorska <jason@metaluna.io>
2021-04-26 14:45:57 +0200

    Make multi-browser extension

--- asyncgo.js
+++ asyncgo.js
@@ -3,8 +3,8 @@
 asyncgo_team_id = 1
 
 function onCreated() {
-  if (browser.runtime.lastError) {
-    console.log(`Error: ${browser.runtime.lastError}`);
+  if (chrome.runtime.lastError) {
+    console.log(`Error: ${chrome.runtime.lastError}`);
   } else {
     console.log("Item created successfully");
   }
@@ -14,13 +14,13 @@ function onError(error) {
   console.log(`Error: ${error}`);
 }
 
-browser.menus.create({
+chrome.contextMenus.create({
   id: "save-selection",
   title: "Create topic from selection",
   contexts: ["selection"]
 }, onCreated);
 
-browser.menus.create({
+chrome.contextMenus.create({
   id: "save-page",
   title: "Create topic from page",
   contexts: ["page"]
@@ -30,15 +30,15 @@ function buildUrl(context, selection) {
    return 'https://app.asyncgo.com/teams/' + asyncgo_team_id + '/topics/new?context=' + encodeURIComponent(context) + '&selection=' + encodeURIComponent(selection) + ''
 }
 
-browser.menus.onClicked.addListener((info, tab) => {
+chrome.contextMenus.onClicked.addListener((info, tab) => {
   switch (info.menuItemId) {
     case "save-selection":
-      browser.tabs.create({url: buildUrl(info.pageUrl, info.selectionText)})
+      chrome.tabs.create({url: buildUrl(info.pageUrl, info.selectionText)})
       console.log(info.pageUrl);
       console.log(info.selectionText);
       break;
     case "save-page":
-      browser.tabs.create({url: buildUrl(info.pageUrl, '')})
+      chrome.tabs.create({url: buildUrl(info.pageUrl, '')})
       console.log(info.pageUrl);
       break;
   }
