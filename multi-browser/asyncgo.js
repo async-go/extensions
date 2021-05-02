ab57656e3160ef45f2a9aed1876dc3b4a80a7a9c
Jason Yavorska <jason@metaluna.io>
2021-05-02 10:51:26 +0200

    Remove logging

--- multi-browser/asyncgo.js
+++ multi-browser/asyncgo.js
@@ -1,15 +1,3 @@
-function onCreated() {
-  if (chrome.runtime.lastError) {
-    console.log(`Error: ${chrome.runtime.lastError}`);
-  } else {
-    console.log("Item created successfully");
-  }
-}
-
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
-
 chrome.contextMenus.create({
   id: "save-selection",
   title: "Create topic from selection",
@@ -30,12 +18,9 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   switch (info.menuItemId) {
     case "save-selection":
       chrome.tabs.create({url: buildUrl(info.pageUrl, info.selectionText)})
-      console.log(info.pageUrl);
-      console.log(info.selectionText);
       break;
     case "save-page":
       chrome.tabs.create({url: buildUrl(info.pageUrl, '')})
-      console.log(info.pageUrl);
       break;
   }
 });
