7b6a3f87029204e4728026aa54c9afc973412836
Jason Yavorska <jason@metaluna.io>
2021-05-05 14:42:03 +0200

    Send as post in new tab

--- multi-browser/asyncgo.js
+++ multi-browser/asyncgo.js
@@ -2,25 +2,38 @@ chrome.contextMenus.create({
   id: "save-selection",
   title: "Create topic from selection",
   contexts: ["selection"]
-}, onCreated);
+});
 
 chrome.contextMenus.create({
   id: "save-page",
   title: "Create topic from page",
   contexts: ["page"]
-}, onCreated);
+});
 
-function buildUrl(context, selection) {
-   return 'https://app.asyncgo.com/new/topic?context=' + encodeURIComponent(context) + '&selection=' + encodeURIComponent(selection) + ''
+function openTab (context, selection) {
+  const url = 'http://localhost:3000/extension/new_topic/'
+  const method = 'POST'
+
+  const tab = chrome.tabs.create({url: 'about:blank'});
+
+  const form = JSON.stringify({context: context, selection: selection})
+
+  fetch(url, {
+      method: method,
+      body: form
+      })
+      .catch(err => {
+          tab.close();
+      });
 }
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   switch (info.menuItemId) {
     case "save-selection":
-      chrome.tabs.create({url: buildUrl(info.pageUrl, info.selectionText)})
+      openTab(info.pageUrl, info.selectionText)
       break;
     case "save-page":
-      chrome.tabs.create({url: buildUrl(info.pageUrl, '')})
+      openTab(info.pageUrl, '')
       break;
   }
 });
