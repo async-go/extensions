574290f51db8ffb1384195386074eb24fb6fbe11
Jason Yavorska <jason@metaluna.io>
2021-12-14 14:30:34 +0100

    Prettier

--- multi-browser/asyncgo.js
+++ multi-browser/asyncgo.js
@@ -1,26 +1,32 @@
 chrome.contextMenus.create({
-  id: 'save-selection',
-  title: 'Create topic from selection',
-  contexts: ['selection']
-})
+  id: "save-selection",
+  title: "Create topic from selection",
+  contexts: ["selection"],
+});
 
 chrome.contextMenus.create({
-  id: 'save-page',
-  title: 'Create topic from page',
-  contexts: ['page']
-})
+  id: "save-page",
+  title: "Create topic from page",
+  contexts: ["page"],
+});
 
-function buildUrl (context, selection) {
-  return 'https://app.asyncgo.com/extension/new_topic?context=' + encodeURIComponent(context) + '&selection=' + encodeURIComponent(selection) + ''
+function buildUrl(context, selection) {
+  return (
+    "https://app.asyncgo.com/extension/new_topic?context=" +
+    encodeURIComponent(context) +
+    "&selection=" +
+    encodeURIComponent(selection) +
+    ""
+  );
 }
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   switch (info.menuItemId) {
-    case 'save-selection':
-      chrome.tabs.create({ url: buildUrl(info.pageUrl, info.selectionText) })
-      break
-    case 'save-page':
-      chrome.tabs.create({ url: buildUrl(info.pageUrl, '') })
-      break
+    case "save-selection":
+      chrome.tabs.create({ url: buildUrl(info.pageUrl, info.selectionText) });
+      break;
+    case "save-page":
+      chrome.tabs.create({ url: buildUrl(info.pageUrl, "") });
+      break;
   }
-})
+});
